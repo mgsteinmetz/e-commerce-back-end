546239e24530d83a6165affc5cb27698ba360793
Mike Steinmetz <mikesteinmetz00@gmail.com>
2021-07-14 18:28:15 -0500

    finding all categories // finding individual categories

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -5,14 +5,56 @@ const { Category, Product } = require('../../models');
 
 router.get('/', (req, res) => {
   // find all categories
+  try {
+    const categoryData = await Category.findAll( {
   // be sure to include its associated Products
+      include: [
+        { 
+          model: Category,
+          model: Product 
+        }
+      ],
+      attributes: {
+        include: [
+          [
+            sequelize.literal(`SELECT COUNT(*) FROM category AND product = id`),
+            'product',
+          ],
+        ],
+      },
+    });
+    res.status(200).json(categoryData);
+  } catch (err) {
+    res.status(200).json(err);
+  }
 });
 
 router.get('/:id', (req, res) => {
   // find one category by its `id` value
+  try {
+    const oneCategory = await Category.findByPk(req.params.id, { 
   // be sure to include its associated Products
+      include: [
+        { 
+          model: Category,
+          model: Product 
+        }
+      ],
+      attributes: {
+        include: [
+          [
+            sequelize.literal(`SELECT COUNT(*) FROM category AND product = id`),
+            'theSuperShortBooks',
+          ],
+        ],
+      },
+    });
+    res.status(200).json(oneCategory);
+  } catch (err) {
+    res.status(200).json(err);
+  }
 });
-
+  
 router.post('/', (req, res) => {
   // create a new category
 });

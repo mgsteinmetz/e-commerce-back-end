59cf6271dc4ff09f4f41e307c461755fc2e354d3
Mike Steinmetz <mikesteinmetz00@gmail.com>
2021-07-15 20:41:13 -0500

    adding category_id into column // adding in NOT NULL and AUTO INCREMENT

--- models/Product.js
+++ models/Product.js
@@ -12,17 +12,37 @@ Product.init(
     // define columns
     id: {
       type: DataTypes.INTEGER,
+      allowNull: false,
+      autoIncrement: true,
       primaryKey: true
     },
     product_name: {
       type: DataTypes.STRING,
+      allowNull: false
     },
     price: {
       type: DataTypes.DECIMAL,
+      allowNull: false,
+      validate: {
+        isDecimal: true
+      },
     },
     stock: {
       type: DataTypes.INTEGER,
-    }
+      allowNull: false,
+      defaultValue: 10,
+      validate: {
+        isNumeric: true
+      },
+    },
+    category_id: {
+      type: DATATYPES.INTEGER,
+      allowNull: false,
+      references: {
+        model: 'category',
+        id: category.id
+      },
+    },
   },
   {
     sequelize,
